e746309b2457e18e08f8cc403ae69623628d7575
DESKTOP-CTTI7B5\shixingyu <shixingyu1990@163.com>
2022-05-02 20:40:30 +0800

    init add

---
+++ apiget/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# from django.shortcuts import  render
+# from django.http.response import HttpResponse
+# Create your views here.
+
+# def homePage(request):
+#     return HttpResponse("<h1>This is home page</h1>")
